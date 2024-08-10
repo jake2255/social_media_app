f76a459028fcb389b2ee343f4047dcb7cb76337e
jake2255 <157094404+jake2255@users.noreply.github.com>
2024-08-10 12:12:16 -0500

    modify model so images are the same size, include date on posts

--- social_media/models.py
+++ social_media/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django_resized import ResizedImageField
 
 class Post(models.Model):
     """A post the user makes to the app."""
     caption = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='social_media/images/')
+    image = ResizedImageField(size=[250,250], upload_to='social_media/images/')
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
